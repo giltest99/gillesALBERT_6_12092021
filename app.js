1a20b632589310fb01e97fbef944e8e7bade2a3a
gilles <sellig.blater@free.com>
2021-09-18 15:38:48 +0200

    Add helmet

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-/* const bodyParser = require('body-parser'); */
+const helmet = require("helmet");
 const mongoose = require('mongoose');
 const path = require('path');
 
 // User routes
 const userRoutes = require('./routes/user');
 
+// Middlewares, app
 const app = express();
+app.use(helmet());
 app.use(express.json());
 
 // Connect to mongoDb
@@ -17,15 +19,13 @@ mongoose.connect(process.env.DB_CONNECT,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  // CORS
+// CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-
-/* app.use(bodyParser.json()); */
 
 // Route to login & signup
 app.use('/api/auth', userRoutes);
