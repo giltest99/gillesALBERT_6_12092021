b5fc1e4fb058b59b5ea77d15bd1658adf76c4c07
gilles <sellig.blater@free.com>
2021-09-19 12:15:32 +0200

    Add feature : delete sauce image when image sauce is  modified

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -52,16 +52,35 @@ exports.getOneSauce = (req, res, next) => {
   );
 };
 
-// modify sauce
+// Modify sauce
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ?
     {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-    .catch(error => res.status(400).json({ error }));
+
+    // Delete image from ./images when sauce image is changed
+    if (req.file) {
+      Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          const filename = sauce.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
+            Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+              .then(() => { res.status(200).json({ message: 'Sauce mise à jour!' }); })
+              .catch((error) => { res.status(400).json({ error }); });
+          })
+        })
+        .catch((error) => { res.status(500).json({ error }); });
+  
+    } 
+    else {
+      Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce mise à jour!' }))
+        .catch((error) => res.status(400).json({ error }));
+    }
+
+
 };
 
 // Delete sauce
